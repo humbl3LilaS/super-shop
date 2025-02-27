8e43b45b642f57f3b4858c51e49d5dedb405dfa0
humbl3LilaS <sabishinekobebe@gmail.com>
2025-02-27 21:40:20 +0630

    setup: formatter and linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,58 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import prettierLint from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier/recommended";
+import importPlugin from "eslint-plugin-import";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    prettierLint,
+    prettierPlugin,
+    importPlugin,
+    {
+        rules: {
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    args: "all",
+                    argsIgnorePattern: "^_",
+                    caughtErrors: "all",
+                    caughtErrorsIgnorePattern: "^_",
+                    destructuredArrayIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    ignoreRestSiblings: true,
+                },
+            ],
+            "import/order": [
+                "error",
+                {
+                    "newlines-between": "always",
+                    groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+                    alphabetize: {
+                        order: "asc",
+                        caseInsensitive: true,
+                    },
+                },
+            ],
+            quotes: [
+                "error",
+                "double",
+                {
+                    allowTemplateLiterals: true,
+                },
+            ],
+            semi: ["error", "always"],
+            "comma-dangle": ["error", "always-multiline"],
+        },
+    },
 ];
 
 export default eslintConfig;
