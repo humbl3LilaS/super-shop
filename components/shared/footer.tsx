6c9ed0b2edcf65198c6c7b79f6aaa99bbeb7a845
humbl3LilaS <sabishinekobebe@gmail.com>
2025-03-01 21:23:39 +0630

    add header and footer

---
+++ components/shared/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    const currentYear = new Date().getFullYear();
+    return (
+        <footer className={"border-t"}>
+            <div className={"p-5 flex-center text-sm font-semibold text-black/50"}>
+                &copy;{currentYear} SuperStore. All Rights Reserved.
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
