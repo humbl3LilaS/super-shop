4f14d7208a95583a92783f25af15c69caef75675
humbl3LilaS <sabishinekobebe@gmail.com>
2025-03-05 21:22:53 +0630

    add user role in the session and jwt token

--- auth.ts
+++ auth.ts
@@ -3,31 +3,33 @@ import { eq } from "drizzle-orm";
 import NextAuth, { User } from "next-auth";
 import CredentialProvider from "next-auth/providers/credentials";
 
-import { users } from "@/database/schema";
+import { IUserRole, users } from "@/database/schema";
 
 import { db } from "./database/drizzle";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     session: {
         strategy: "jwt",
+        maxAge: 24 * 60 * 60,
     },
     providers: [
         CredentialProvider({
+            credentials: { email: { type: "string" }, password: { type: "string" } },
             async authorize(credentials) {
-                if (!credentials?.email || !credentials?.password) {
+                if (!credentials.email || !credentials.password) {
                     return null;
                 }
                 const [user] = await db
                     .select()
                     .from(users)
-                    .where(eq(users.email, credentials?.email?.toString()));
+                    .where(eq(users.email, credentials.email.toString()));
 
                 if (!user) {
                     return null;
                 }
 
                 const isPasswordCorrect = await compare(
-                    credentials?.password.toString(),
+                    credentials.password.toString(),
                     user.password,
                 );
                 if (!isPasswordCorrect) {
@@ -38,6 +40,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                     id: user.id,
                     email: user.email,
                     name: user.name,
+                    role: user.role,
                 } as User;
             },
         }),
@@ -50,6 +53,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             if (user) {
                 token.id = user.id;
                 token.email = user.email;
+                token.role = user.role;
             }
             return token;
         },
@@ -61,6 +65,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
                         ...session.user,
                         id: token.id.toString(),
                         name: token.name as string,
+                        role: token.role as IUserRole,
                     },
                 };
             }
