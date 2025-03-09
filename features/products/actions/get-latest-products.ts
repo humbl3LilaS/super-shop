6febb1690736ee55d320f1a3eec599b2c7b5ce43
humbl3LilaS <sabishinekobebe@gmail.com>
2025-03-09 22:06:10 +0630

    add product detail page

--- features/products/actions/get-latest-products.ts
+++ features/products/actions/get-latest-products.ts
@@ -1,16 +1,10 @@
 "use server";
 
-import { desc } from "drizzle-orm";
-
 import { db } from "@/database/drizzle";
 import { products } from "@/database/schema";
 
 export const getLatestProducts = async () => {
-    const latestProducts = await db
-        .select()
-        .from(products)
-        .limit(4)
-        .orderBy(desc(products.createdAt));
+    const latestProducts = await db.select().from(products).limit(4);
     if (!latestProducts) {
         return [];
     }
---
+++ features/products/components/product-images.tsx
@@ -0,0 +1,40 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
+import { cn } from "@/lib/utils";
+
+const ProductImages = ({ images }: { images: string[] }) => {
+    const [activeIdx, setActiveIdx] = useState(0);
+    return (
+        <div className={"col-span-1 "}>
+            <div className={"space-y-4 "}>
+                <Image
+                    src={images[activeIdx]}
+                    alt={"product detail image"}
+                    width={500}
+                    height={500}
+                    className={"min-h-[300px] mx-auto object-cover object-center"}
+                />
+                <div className={"flex items-center gap-x-4"}>
+                    {images.map((image, idx) => (
+                        <div
+                            key={image}
+                            className={cn(
+                                "aspect-square w-24 p-1 rounded-md overflow-hidden border hover:border-orange-800 transition-colors duration-500",
+                                activeIdx === idx && "border-orange-500",
+                            )}
+                            onClick={() => {
+                                setActiveIdx(idx);
+                            }}
+                        >
+                            <Image src={image} alt={"product image"} width={100} height={100} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductImages;
