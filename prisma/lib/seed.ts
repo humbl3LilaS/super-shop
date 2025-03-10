5a7d4c3707fb9c7795519cec6c648fed065c44b0
humbl3LilaS <sabishinekobebe@gmail.com>
2025-03-10 21:29:46 +0630

    add: user seeding

--- prisma/lib/seed.ts
+++ prisma/lib/seed.ts
@@ -1,13 +1,36 @@
 import { PrismaClient } from "@prisma/client";
+import { hashSync } from "bcryptjs";
 
-import { SAMPLE_DATA } from "@/lib/constants";
+import { SAMPLE_DATA, SAMPLE_USER } from "@/lib/constants";
+
+const prisma = new PrismaClient();
+
+async function cleanUp() {
+    console.log("Cleaning up");
+    await prisma.product.deleteMany();
+    await prisma.account.deleteMany();
+    await prisma.session.deleteMany();
+    await prisma.verificationToken.deleteMany();
+    await prisma.user.deleteMany();
+    console.log("Cleaning up Complete");
+}
 
 async function main() {
-    console.log("Seeding...");
-    const prisma = new PrismaClient();
-    await prisma.product.deleteMany();
+    await cleanUp();
 
+    console.log("Seeding...");
     await prisma.product.createMany({ data: SAMPLE_DATA });
+
+    const processedUsers = SAMPLE_USER.map((user) => {
+        const password = hashSync(user.password, 10);
+        return {
+            ...user,
+            password,
+        };
+    });
+
+    await prisma.user.createMany({ data: processedUsers });
+
     console.log("Seeding... Complete");
 }
 
